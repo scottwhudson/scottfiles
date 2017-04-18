75aaeecc78f99860d64e2618c30b9d1319a21a38
swhudson <scott.w.hudson@gmail.com>
2017-04-17 20:45:59 -0500

    rm vim git repo

---
+++ foo/README.md
@@ -0,0 +1,7 @@
+# Scott's Vim Config
+
+Plugins used:
+- https://github.com/tpope/vim-pathogen
+- https://github.com/kien/ctrlp.vim
+- https://github.com/vim-ruby/vim-ruby
+
---
+++ foo/install.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+puts "writing .vimrc"
+`cp vimrc ~/.vimrc`
+
+puts "cloning pathogen"
+`git clone https://github.com/tpope/vim-pathogen.git ~/.vim/autoload/pathogen.vim`
+
+puts "cloning Ctrl-P"
+`git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim`
+
+puts "cloning Nerdtree"
+`git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree`
+
+puts "cloning Vim Unimpaired"
+`git clone git://github.com/tpope/vim-unimpaired.git ~/.vim/bundle/vim-unimpaired`
+
+puts "cloning Vim Solarized"
+`git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized`
