941a0022042f2c9b0ab4894bf0989a447fa9dba6
Scott.Hudson <scott.hudson@ScottHudson.local>
2020-03-10 10:24:30 -0500

    rm cruft

--- brew.sh
+++ brew.sh
@@ -13,59 +13,7 @@ brew update
 
 # Upgrade any already-installed formulae.
 brew upgrade --all
-
-# Install GNU core utilities (those that come with OS X are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
-
-# Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`.
-brew install bash
-brew tap homebrew/versions
 brew install bash-completion2
-
-# Install `wget` with IRI support.
-brew install wget --with-iri
-
-# Install more recent versions of some OS X tools.
-brew install macvim --override-system-vi
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
-
-# Install other useful binaries.
-brew install ack
-brew install dark-mode
-brew install git
-brew install git-lfs
-brew install imagemagick --with-webp
-brew install mysql
-brew install postgresql
-brew install python
-brew install python3
-brew install ngrep
-brew install pv
-brew install rbenv
-brew install tmux
-brew install tree
-
-# Install casks
-brew tap caskroom/cask
-brew cask install atom
-brew cask install spectacle
-brew cask install slack
-brew cask install google-chrome
-brew cask install firefox
-brew cask install postman
-brew cask install backblaze
-brew cask install iterm2
-brew cask install lastpass
-brew cask install spotify
-brew cask install libreoffice
-
-# background services
-brew services start mysql
 
 # terminal font
 brew tap caskroom/fonts
